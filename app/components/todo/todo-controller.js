c73689fd269699478a2b56fc0b5045b50c9722e9
Zmixon91 <zmixon91@gmail.com>
2016-01-29 11:02:33 -0700

    I love apples!

--- app/components/todo/todo-controller.js
+++ app/components/todo/todo-controller.js
@@ -1,4 +1,8 @@
 app.controller('TodoController', function () {
     var vm = this;
     vm.apple = "I love Apples";
+    vm.favoriteFoods = [];
+    vm.addFood = function () {
+        vm.favoriteFoods.push(vm.apple);
+    }
 });
